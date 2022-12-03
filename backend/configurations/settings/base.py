087474fa13e0585c5efc5097327098e2cd572fca
Bruno Alves <bruno6821@gmail.com>
2022-12-03 11:55:26 -0300

    Configurando os Logs do servidor

--- backend/configurations/settings/base.py
+++ backend/configurations/settings/base.py
@@ -1,3 +1,6 @@
+from django.utils.log import DEFAULT_LOGGING
+import logging.config
+import logging
 from pathlib import Path
 import environ
 
@@ -21,7 +24,7 @@ DJANGO_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.sites",
+    'django.contrib.sites'
 ]
 
 SITE_ID = 1
@@ -112,3 +115,47 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = "INFO"
+
+logging.config.dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        "file": {
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "console",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "formatter": "file",
+            "filename": "logs/api_backend.log",
+        },
+        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+    },
+    "loggers": {
+        "": {
+            "level": "INFO",
+            "handlers": ["console", "file"],
+            "propagate": False
+        },
+        "apps": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False
+        },
+        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+    }
+})
