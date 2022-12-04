729fdddf15f6fc46ae3f713bc06f74d3896df92c
Bruno Alves <bruno6821@gmail.com>
2022-12-04 03:48:08 -0300

    implementação das Urls do Profile

---
+++ backend/apps/profiles/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (AgenteListeAPIView, GetProfileAPIView,
+                    TopAgenteListeAPIView, UpdateProfileAPIView)
+
+urlpatterns = [
+    path('profile/', GetProfileAPIView.as_view(), name='get_profile'),
+    path('update/<str:username>/', UpdateProfileAPIView.as_view(),
+         name='update_profile'),
+    path('agents/list/', AgenteListeAPIView.as_view(), name='agents_list'),
+    path('top-agents/list/', TopAgenteListeAPIView.as_view(),
+         name='top_agents_list'),
+]
