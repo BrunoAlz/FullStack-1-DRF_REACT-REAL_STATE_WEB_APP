d7feed922a59cb9245ec13ddafd5c093aa5660d1
Bruno Alves <bruno6821@gmail.com>
2022-12-03 23:22:05 -0300

    Implementando o User Serializer, Disponibilizando o env.example

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -10,5 +10,63 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     gender = serializers.CharField(
+        # Inner Join no Profile para buscar dados do Profile
         source='profile.gender'
     )
+
+    phone_number = PhoneNumberField(
+        source='profile.phone_number'
+    )
+
+    profile_photo = serializers.ImageField(
+        source='profile.profile_photo'
+    )
+
+    country = CountryField(
+        source='profile.country'
+    )
+
+    city = serializers.CharField(
+        source='profile.city'
+    )
+
+    top_seller = serializers.BooleanField(
+        source='profile.top_seller'
+    )
+
+    first_name = serializers.SerializerMethodField()
+    top_seller = serializers.SerializerMethodField()
+
+    full_name = serializers.SerializerMethodField(
+        # Chama a @Property do User model para buscar o Nome completo
+        source='get_full_name'
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email',
+            'first_name', 'last_name', 'full_name',
+            'gender', 'phone_number', 'profile_photo',
+            'country', 'city', 'top_seller',]
+
+    def get_first_name(self, obj):
+        return obj.first_name.title()
+
+    def get_last_name(self, obj):
+        return obj.last_name.title()
+
+    # Se o Usuário da Requisição,  for um super Usuário, será exibido uma representação
+    def to_representation(self, instance):
+        representation = super(UserSerializer, self).to_representation(instance)
+        if representation.is_superuser:
+            representation['admin'] = True
+        return representation
+
+
+class CreateUserSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = [
+            'id', 'username', 'email',
+            'first_name', 'last_name', 'password']
