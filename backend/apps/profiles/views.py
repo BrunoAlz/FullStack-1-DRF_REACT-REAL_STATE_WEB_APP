91acbbb4add2c834ea04ec8d6af02e3b4f7e9db9
Bruno Alves <bruno6821@gmail.com>
2022-12-04 03:44:05 -0300

    Implementando as APIViews do Profile

--- backend/apps/profiles/views.py
+++ backend/apps/profiles/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .exceptions import NotYourProfile, ProfileNotFound
+from .models import Profile
+from .renderers import ProfileJSONRenderer
+from .serializers import ProfileSerializer, UpdateProfileSerializer
+
+"""
+    Diferença entre o CBV e o FVB API, para API, classes são bem mais
+    simples de usar, no Django é meio chato, por que adiciona muito código
+
+    from rest_framework import generics, permissions
+
+    @api_view("GET")
+    @permission_classes((permissions.IsAuthenticated))
+    def get_all_agents(request):
+        agents = Profile.objects.filter(is_agente=True)
+        serializer=ProfileSerializer(agents, many=True)
+        name_spaced_response={'agents': serializer.data}
+        return Response(name_spaced_response, status.HTTP_200_ok)
+"""
+
+
+class AgenteListeAPIView(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated)
+    queryset = Profile.objects.filter(is_agente=True)
+    serializer_class = ProfileSerializer
+
+
+class TopAgenteListeAPIView(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated)
+    queryset = Profile.objects.filter(top_agent=True)
+    serializer_class = ProfileSerializer
+
+
+class GetProfileAPIView(APIView):
+    permission_classes = (permissions.IsAuthenticated)
+    renderer_classes = [ProfileJSONRenderer]
+
+    def get(self, request):
+        user = self.request.user
+        user_profile = Profile.objects.get(user=user)
+        serializer = ProfileSerializer(user_profile, context={
+            'request': request
+        })
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UpdateProfileAPIView(APIView):
+    permission_classes = (permissions.IsAuthenticated)
+    renderer_classes = [ProfileJSONRenderer]
+
+    serializer_class = UpdateProfileSerializer
+
+    def patch(self, request, username):
+        try:
+            Profile.objects.get(username=username)
+        except Profile.DoesNotExist:
+            raise ProfileNotFound
+
+        user_name = request.user.username
+        if user_name != username:
+            raise NotYourProfile
+
+        data = request.data
+        serializer = UpdateProfileSerializer(
+            instance=request.user.profile,
+            data=data,
+            partial=True
+        )
+
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
