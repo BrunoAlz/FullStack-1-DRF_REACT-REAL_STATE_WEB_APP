8fb4a6b428b2e3919adb3fba034a9bcb8de9e140
Bruno Alves <bruno6821@gmail.com>
2022-12-05 16:41:27 -0300

    Criando o componente Title

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,19 @@
+import Helmet from 'react-helmet'
+
+const Title = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title> {title} </title>
+      <meta name="description" content={description}/>
+      <meta name="keywords" content={keywords}/>
+    </Helmet>
+  )
+}
+
+Title.defautoProps = {
+  title: "Bem vindo ao Moderna Houses",
+  description: "Vendemos, alugamos ou compramos a casa dos seus sonhos!",
+  keywords: "Casas, Ap's, Estudios",
+}
+
+export default Title
