70a561de74cfff65120100a970caf4da7830b18f
Bruno Alves <bruno6821@gmail.com>
2022-12-05 16:24:34 -0300

    Configurando o  auth Reducer no store

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import PropertyReducer from '../slices/properties/PropertySlice';
+import authReducer from '../slices/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
     properties: PropertyReducer,
+    auth: authReducer,
   },
 });
