9c215a47acdecfc96c9ce40e87474187a53c1fd9
Bruno Alves <bruno6821@gmail.com>
2022-12-05 17:03:19 -0300

    Criando a Pagina de Propriedades

---
+++ frontend/src/pages/PropertiesPage.jsx
@@ -0,0 +1,17 @@
+import { Col, Container, Row } from 'react-boostrap'
+
+const PropertiesPage = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="mg-top">
+            <h1>Propriedades disponíveis</h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default PropertiesPage
